71562c86f8c4dc02ee76ce85256cfb584983050a
OHYA Satoshi <conatus.11099@gmail.com>
2024-06-02 20:17:00 +0900

    fix

--- ICM20948_USER.cpp
+++ ICM20948_USER.cpp
@@ -7,13 +7,3 @@
 
 #include "ICM20948_USER.h"
 
-void ICM20948_USER::init(){
-	icm20948.reset();
-	icm20948.pwrmgmt2(icm20948.DISABLE_SENSORS);
-	icm20948.accelConfig(ICM20948::AccelSensitivity::SENS_2G,false,0);
-	icm20948.gyroConfig(ICM20948::GyroSensitivity::SENS_500, false, 0);
-	icm20948.pwrmgmt2(icm20948.ENABLE_SENSORS);
-	icm20948.intPinConfig(0b01110000);
-	icm20948.intenable();
-}
-
